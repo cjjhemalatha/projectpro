98263d1af9d944cca7937637f2a0f38e5c016993
cjjhemalatha <cjjhemalatha@gmail.com>
2021-08-27 16:25:44 +0530

    first commit

---
+++ src/main/java/com/revature/project03/model/Family.java
@@ -0,0 +1,5 @@
+package com.revature.project03.model;
+
+public class Family {
+
+}
---
+++ src/main/java/com/revature/project03/model/Patient.java
@@ -0,0 +1,33 @@
+package com.revature.project03.model;
+//
+//import javax.persistence.Entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+//@Entity
+//@Table(name = "Patient")
+public class Patient {
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private int p_id;
+//	
+//	private String firstName;
+//	private String lastName;
+//	private int age;
+//	private String gender;
+//	private String mobileNo;
+//	private String address;
+//	private String email_id;
+//	
+//	@OneToMany(cascade=CascadeType.ALL, mappedBy = "patient")
+//	private List<Family> members=new ArrayList<>();
+//	
+}
